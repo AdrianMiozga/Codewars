d96cc51373a00399e2097a64d9410c2df1e3ed61
Adrian Miozga <AdrianMiozga@outlook.com>
2019-09-08 19:04:33 +0000

    Add Solution 4.java
    Kata name: Square(n) Sum

---
+++ 8 kyu/Squaren Sum/Solution 4.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+
+public class Kata {
+  public static int squareSum(int[] n) { 
+    int sum = 0;
+    
+    for (int number : n) {
+      sum += Math.pow(number, 2);
+    }
+    return sum;
+  }
+}
