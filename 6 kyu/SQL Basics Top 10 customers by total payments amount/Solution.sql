2bc318bbfaad9dc250d8ddf5d4966e29928d8796
Adrian Miozga <AdrianMiozga@outlook.com>
2020-04-09 18:55:09 +0000

    Add Solution.sql
    Kata name: SQL Basics: Top 10 customers by total payments amount

---
+++ 6 kyu/SQL Basics Top 10 customers by total payments amount/Solution.sql
@@ -0,0 +1,5 @@
+SELECT customer.customer_id, customer.email, COUNT(*) AS payments_count, SUM(payment.amount)::FLOAT AS total_amount
+  FROM customer
+  INNER JOIN payment ON customer.customer_id = payment.customer_id
+  GROUP BY customer.customer_id
+  ORDER BY total_amount DESC LIMIT 10;
