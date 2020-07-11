36ca2738f3db5f24211822693a137948717c279b
Adrian Miozga <AdrianMiozga@outlook.com>
2020-07-11 22:44:31 +0000

    Add Solution 1.java
    Kata name: Abbreviate a Two Word Name

---
+++ 8 kyu/Abbreviate a Two Word Name/Solution 1.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class AbbreviateTwoWords {
+  public static String abbrevName(String name) {
+    return Arrays.stream(name.split(" "))
+      .map(String::toUpperCase)
+      .map(word -> word.substring(0, 1))
+      .collect(Collectors.joining("."));
+  }
+}
