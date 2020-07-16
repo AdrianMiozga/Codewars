a878e6bc616b1bd1356c1aa4fb816e969f20deec
Adrian Miozga <AdrianMiozga@outlook.com>
2020-07-16 15:44:51 +0000

    Add Solution.java
    Kata name: Remove duplicate words

---
+++ 7 kyu/Remove duplicate words/Solution.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+  public static String removeDuplicateWords(String s) {
+    return Arrays.stream(s.split(" ")).distinct().collect(Collectors.joining(" "));
+  }
+}
