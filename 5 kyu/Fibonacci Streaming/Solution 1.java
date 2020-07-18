0a9a950d368e018ce0932236b562d6e922b93a16
Adrian Miozga <AdrianMiozga@outlook.com>
2020-07-18 04:48:50 +0000

    Add Solution 1.java
    Kata name: Fibonacci Streaming

---
+++ 5 kyu/Fibonacci Streaming/Solution 1.java
@@ -0,0 +1,9 @@
+import java.util.stream.Stream;
+import java.util.stream.IntStream;
+
+public class Utility {
+  public static IntStream generateFibonacciSequence() {
+    return Stream.iterate(new int[] {1, 1}, p -> new int[] {p[1], p[0] + p[1]})
+      .mapToInt(result -> result[0]);
+  }
+}
